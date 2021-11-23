18081888ca22159ce09f012b1d02213ee22d836a
leandro nicolas ovejero <leandroovejero16197@gmail.com>
2021-11-23 15:57:51 -0300

    Login Automatico completo

--- Controller/RegistroController.php
+++ Controller/RegistroController.php
@@ -1,14 +1,17 @@
 <?php
 require_once './Model/UsuarioModel.php';
 require_once './Views/RegistroView.php';
+require_once './Controller/LoginController.php';
 
 class RegistroController{
     private $model;
     private $view;
+    private $login;
 
     function __construct(){
         $this->model = new UsuarioModel();
         $this->view = new RegistroView();
+        $this->login=new LoginController();
     }
 
     function locacionLogin(){
@@ -22,6 +25,6 @@ class RegistroController{
     function Registro(){
             $hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
             $this->model->addUser($_POST['nombre'],$_POST['user'],$hash);
-            $this->locacionLogin();
+            $this->login->IniciarSesion();
     }
 }
