0e239dee8fcb0e2534e4cd5a0dee85ece8373303
leandro nicolas ovejero <leandroovejero16197@gmail.com>
2021-11-24 17:43:11 -0300

    Base de datos actualizada

--- DB/blogserie.sql
+++ DB/blogserie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2021 a las 04:11:51
+-- Tiempo de generación: 24-11-2021 a las 21:40:54
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.4
 
@@ -37,6 +37,18 @@ CREATE TABLE `comentario` (
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `comentario`
+--
+
+INSERT INTO `comentario` (`id_comentario`, `comentario`, `puntaje`, `id_usuario`, `id_serie`, `fecha`) VALUES
+(15, '2', 3, 1, 4, '2021-11-00'),
+(16, '2', 3, 1, 4, '2021-11-00'),
+(17, '2', 3, 1, 4, '2021-11-01'),
+(18, '1', 3, 7, 4, '2021-11-01'),
+(19, '2', 3, 8, 4, '2021-11-02'),
+(20, '2', 3, 8, 4, '2021-11-02');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +59,14 @@ CREATE TABLE `genero` (
   `id_genero` int(11) NOT NULL,
   `nombreGen` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`id_genero`, `nombreGen`) VALUES
+(20, 'accion'),
+(24, 'asd');
 
 -- --------------------------------------------------------
 
@@ -62,6 +82,13 @@ CREATE TABLE `serie` (
   `id_genero` int(11) NOT NULL,
   `img` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `serie`
+--
+
+INSERT INTO `serie` (`id_serie`, `nombre`, `sinopsis`, `actor_principal`, `id_genero`, `img`) VALUES
+(4, 'asd', '2', 'as', 20, 'publico/images/6199bfddca344ban.png');
 
 -- --------------------------------------------------------
 
@@ -83,8 +110,9 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `user`, `pass`, `admin`) VALUES
 (1, 'leandro', 'leandro', '$2y$10$WA0/5bx1uT6AJjZS2zLzH.hEHwhnu9jK3WK7.hKPMeCbd9Yj5S8He', 1),
-(3, 'comun', 'comun', '$2y$10$uq7WCmpynPvOnWLjCIZyuOitHJz6N869yRAYGVkiaYLjtrL5Oltjy', 0),
-(4, 'cosa1', 'cosa1', '$2y$10$CnefDPlaNC1zdFPolWdg.eOLOP/QYpLRKnYID.RjC/zT04ftOvLDW', 0);
+(2, 'test', 'test', '$2y$10$213U0JxNgF6VPx/m0t8K4OqRlgoaPKvRRh4bIFQ4l14hLHqvG4fom', 1),
+(7, 'hola', 'hola', '$2y$10$4uf9xjGxwgG8/omYuoODiu8cuJaLvQIP4FwyV26zolb8DHbbhBmry', 0),
+(8, 'stella', 'stella', '$2y$10$eJ3a9S0B9n.D/fZTINICd.xix2/XP9qggLsFbxOMWjXxv27OQemxW', 0);
 
 --
 -- Índices para tablas volcadas
@@ -96,7 +124,7 @@ INSERT INTO `usuario` (`id_usuario`, `nombre`, `user`, `pass`, `admin`) VALUES
 ALTER TABLE `comentario`
   ADD PRIMARY KEY (`id_comentario`),
   ADD KEY `fk_usuario` (`id_usuario`),
-  ADD KEY `fk_genero` (`id_serie`);
+  ADD KEY `fk_serie` (`id_serie`) USING BTREE;
 
 --
 -- Indices de la tabla `genero`
@@ -125,25 +153,25 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `comentario`
 --
 ALTER TABLE `comentario`
-  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `serie`
 --
 ALTER TABLE `serie`
-  MODIFY `id_serie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id_serie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
